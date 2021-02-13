5ed61ff6401f903382f3351d792054657f14ca0d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-13 15:04:53 +0000

    added token

--- src/main/java/umudev/witch/discordbot/utils/configs.java
+++ src/main/java/umudev/witch/discordbot/utils/configs.java
@@ -2,7 +2,7 @@ package umudev.witch.discordbot.utils;
 
 public class configs {
   // put token here
-  public static String token = "";
+  public static String token = "NzQ4NzA1NTc2MDM2MzM1NjI2.X0hUZw.LKuwtIMnJF3aGhk0SZfJDcJQSyo";
   public static String prefix = "w!";
 
 }
